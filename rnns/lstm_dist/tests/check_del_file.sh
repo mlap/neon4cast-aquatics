5785f09067441f7e4e01e864fbe81636ad45bb96
mlap <marcuslapeyrolerie@me.com>
2021-07-23 15:32:20 +0000

    Deleting old folder

--- rnns/lstm_dist/tests/check_del_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-  
-check_del_file(){
-        if [ ! -f "$1" ]; then
-                echo -e "\n\nError: $1 does not exist."
-                exit 1
-        fi
-        rm $1
-}
--- rnns/lstm_dist/tests/test-tune.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source tests/check_del_file.sh
-
-python rnn_tune.py --n-trials 5 --variable wt
-check_del_file studies/trash.db
-
-python rnn_tune.py --n-trials 5
-check_del_file studies/trash.db
